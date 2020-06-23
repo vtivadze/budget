3775b0d3d099587c167b9ab5a5efd3789b4b341e
Vakhtang Tivadze <vaxotivadze@gmail.com>
2020-06-24 01:45:58 +0400

    Make third homework

--- scripts.js
+++ scripts.js
@@ -1,237 +1,250 @@
-'use strict';
+(function () {
+    'use strict';
 
-let appData = {
-    budget: 0,
-    expenses: {},
-    optionalExpenses: {},
-    income: [],
-    timeData: '',
-    savings: true,
-};
+    let appData = {
+        budget: 0,
+        expenses: {},
+        optionalExpenses: {},
+        income: [],
+        timeData: '',
+        savings: true,
 
-start();
+        messages: {
+            monthlyBudget: 'What is your budget for a month?',
+            timeData: 'Enter date in format YYYY-MM-DD',
+            chooseExpenses: 'Such obligatory expenses item already exists!',
+            lowLevel: 'Minimal wealth level!',
+            middleLevel: 'Middle wealth level!',
+            highLevel: 'High wealth level!',
+            errorLevel: 'An error has occured!',
+            chooseIncome: 'List your additional income items (through commas)',
+            expenseItem: 'Enter obligatory expenses item name for this month:',
+            expenseAmount: 'How much will it cost for this month?',
+            saveAmount: 'How much does it your accumulations?',
+            savePercent: 'How much does it percent of your deposit?',
+            optExpenseItem: 'What is your optional expense item?',
+            incomeElse: 'May be something else?',
+        },
 
-alert( detectLevel() );
+        defaults: {
+            monthlyBudget: 'Enter positive number value',
+            timeData: 'Format is obligatory',
+            expenseAmount: 'Enter positive number value',
+            saveAmount: 'Enter number value!',
+            savePercent: 'Enter number value',
+            incomeElse: '',
+            chooseIncome: '',
+            expenseItem: '',
+            optExpenseItem: '',
+        },
 
-// chooseOptExpenses();
-
-//  app functions
-function start() {
-    askMonthlyBudget();
-    askTimeData();
-    chooseExpenses();
-    detectDayBudget();
-    checkSavings();
-}
-
-function askMonthlyBudget() {
-    const message = 'What is your budget for a month?';
-    const defaultValue = 'Enter positive number value';
-
-    let money;
-
-    do {
-        money = prompt(message, defaultValue);
-    } while  ( !isCorrectNumber(money) );
-
-    appData.budget = + parseFloat(money).toFixed(2);
-}
-
-function askTimeData() {
-    const message = 'Enter date in format YYYY-MM-DD';
-    const defaultValue = 'Format is obligatory';
-
-    let time;
-
-    do {
-        time = prompt(message, defaultValue);
-    } while  ( !hasCorrectTimeFormat(time) );
-
-    appData.timeData = time;
-
-}
-
-function chooseExpenses() {
-    const expensesCount = 2;
-    const message = 'Such obligatory expenses item already exists!';
-
-    let expenses = {};
-    let expenseItem;
-    let expenseAmount;
-
-    for (let i = 0; i < expensesCount; i++) {
-
-        expenseItem = askExpenseItem(i);
+        settings: {
+            expensesCount: 2,
+            daysInMonth: 30,
+            lowBoundary: 100,
+            highBoundary: 2000,
+            optExpensesCount: 3,
+            decimalDigitCount: 2,
+        },
     
-        if (hasExpenseItem(expenses, expenseItem)) {
-            alert(message);
-            i--;
-            continue;
+        askMonthlyBudget: function () {
+            let money = askForCorrectNumber(this.messages.monthlyBudget, this.defaults.monthlyBudget);
+            appData.budget = + parseFloat(money).toFixed(this.settings.decimalDigitCount);
+        },
+    
+        askTimeData: function () {
+            let time = askForCorrectDate(this.messages.timeData, this.defaults.timeData);
+            appData.timeData = time;
+        },
+    
+        chooseExpenses: function () {
+            let expenses = {};
+            let expenseItem;
+            let expenseAmount;
+        
+            for (let i = 0; i < this.settings.expensesCount; i++) {
+        
+                expenseItem = this.askExpenseItem(i);
+            
+                if (this.hasExpenseItem(expenses, expenseItem)) {
+                    alert(this.messages.chooseExpenses);
+                    i--;
+                    continue;
+                }
+            
+                expenseAmount = this.askExpenseAmount();
+                expenses[expenseItem] = expenseAmount;
+            
+            }
+        
+            appData.expenses = expenses;
+        },
+    
+        detectDayBudget: function () {
+            appData.moneyPerDay = Math.round( (appData.budget / this.settings.daysInMonth) * 100 ) / 100;
+            alert('Daily budget: ' + appData.moneyPerDay);
+        },
+    
+        detectLevel: function () {
+            let moneyPerDay = appData.moneyPerDay;
+        
+            if (moneyPerDay < this.settings.lowBoundary) {
+                return this.messages.lowLevel;
+            } else if (moneyPerDay <= this.settings.highBoundary) {
+                return this.messages.middleLevel;
+            } else if (moneyPerDay > this.settings.highBoundary) {
+                return this.messages.highLevel;
+            } else {
+                return this.messages.errorLevel;
+            }
+
+        },
+    
+        checkSavings: function () {
+            if (appData.savings === true) {
+                let save = this.askSaveAmount();
+                let percent = this.askSavePercent();
+        
+                let monthlyIncome = save / 100 / 12 * percent;
+                appData.monthlyIncome = + parseFloat(monthlyIncome).toFixed(this.settings.decimalDigitCount);
+        
+                alert('Montly income from your depostit is: ' + appData.monthlyIncome);
+            }
+        },
+    
+        chooseOptExpenses: function () {
+            for (let i = 1; i <= this.settings.optExpensesCount; i++) {
+                appData.optionalExpenses[i] = this.askOptExpenseItem();
+            }
+        },
+
+        chooseIncome: function () {
+            let items = askForCorrectString(this.messages.chooseIncome, this.defaults.chooseIncome);
+            appData.income = items.split(', ');
+            appData.income.push(askForCorrectString(this.messages.incomeElse, this.defaults.incomeElse));
+            appData.income.sort();
+
+            let alertMessage = 'Additional income methods: ';
+            appData.income.forEach(function (value, index) {
+                alertMessage += (index + 1) + '. ' + value + '; ';
+            });
+            alertMessage = alertMessage.slice(0, -2) + '.';
+            alert(alertMessage);
+        },
+    
+    
+    
+        askExpenseItem: function (i) {
+            return askForCorrectString(this.messages.expenseItem, this.defaults.expenseItem);
+        },
+        
+        askExpenseAmount: function () {
+            let expenseAmount = askForCorrectNumber(this.messages.expenseAmount, this.defaults.expenseAmount);
+            return + parseFloat(expenseAmount).toFixed(this.settings.decimalDigitCount);
+        },
+
+        hasExpenseItem: function (expenses, expenseName) {
+            return expenses[expenseName] !== undefined;
+        },
+
+        askSaveAmount: function () {
+            let amount = askForCorrectNumber(this.messages.saveAmount, this.defaults.saveAmount);
+            return + parseFloat(amount).toFixed(this.settings.decimalDigitCount);
+        },
+
+        askSavePercent: function () {
+            let percent = askForCorrectNumber(this.messages.savePercent, this.defaults.savePercent);
+            return + parseFloat(percent).toFixed(this.settings.decimalDigitCount);
+        },
+
+        askOptExpenseItem: function () {
+            let item = askForCorrectString(this.messages.optExpenseItem, this.defaults.optExpenseItem);
+            return item;
+        }
+    };
+    
+    // start();
+    
+    // alert( appData.detectLevel() );
+    
+    // appData.chooseOptExpenses();
+    appData.chooseIncome();
+
+    console.log(appData);
+    console.log("Our progrom includs datas: ");
+    for (let data in appData) {
+        console.log(data);
+    }
+    
+    //  app functions
+    function start() {
+        appData.askMonthlyBudget();
+        appData.askTimeData();
+        appData.chooseExpenses();
+        appData.detectDayBudget();
+        appData.checkSavings();
+    }
+    
+    // helper functions
+    function isCorrectNumber(data) {
+        const minNumber = 0;
+        const maxNumber = 5000;
+    
+        let isNumber = data !== null && data !== '' && !isNaN(data);
+        if ( ! isNumber ) {
+            alert('Enter number value!');
+            return false;
         }
     
-        expenseAmount = askExpenseAmount();
+        let isCorrectNumber = data > minNumber && data < maxNumber;
+        if ( ! isCorrectNumber ) {
+            alert('Enter number between ' + minNumber + ' and ' + maxNumber + '!');
+            return false;
+        }
     
-        expenses[expenseItem] = expenseAmount;
+        return true;
+    }
     
+    function hasCorrectTimeFormat(time) {
+        const timeFormat = '^20[0-9]{2}-(0[0-9]{1}|1[0-2]{1})-([0-2]{1}[0-9]{1}|3[0-1]{1})$';
+        const regexpTime = RegExp(timeFormat);
+    
+        return regexpTime.test(time);
+    }
+    
+    function isCorrectString(data) {
+        const allowedStringLength = 200;
+    
+        return (
+            typeof(data) === 'string' &&
+            data !== '' &&
+            data.length <= allowedStringLength
+        );
     }
 
-    appData.expenses = expenses;
-}
-
-function detectDayBudget() {
-    const daysInMonth = 30;
-
-    appData.moneyPerDay = Math.round( (appData.budget / daysInMonth) * 100 ) / 100;
-
-    alert('Daily budget: ' + appData.moneyPerDay);
-}
-
-function detectLevel() {
-    const lowBoundary = 100;
-    const highBoundary = 2000;
-
-    const lowMessage = 'Minimal wealth level!';
-    const middleMessage = 'Middle wealth level!';
-    const highMessage = 'High wealth level!';
-
-    const errorMessage = 'An error has occured!';
-
-    let message;
-    let moneyPerDay = appData.moneyPerDay;
-
-    if (moneyPerDay < lowBoundary) {
-        message = lowMessage;
-    } else if (moneyPerDay <= highBoundary) {
-        message = middleMessage;
-    } else if (moneyPerDay > highBoundary) {
-        message = highMessage;
-    } else {
-        message = errorMessage;
+    function askForCorrectNumber(msg, def) {
+        return askFor(msg, def, isCorrectNumber);
     }
 
-    return message;
-}
-
-function checkSavings() {
-    if (appData.savings === true) {
-        let save = askSaveAmount();
-        let percent = askSavePercent();
-
-        let monthlyIncome = save / 100 / 12 * percent;
-        appData.monthlyIncome = + parseFloat(monthlyIncome).toFixed(2);
-
-        alert('Montly income from your depostit is: ' + appData.monthlyIncome);
-    }
-}
-
-function chooseOptExpenses() {
-    const optExpensesCount = 3;
-
-    for (let i = 1; i <= optExpensesCount; i++) {
-        appData.optionalExpenses[i] = askOptExpenseItem();
-    }
-}
-
-
-// secondary functions
-function askExpenseItem(i) {
-    const message = 'Enter obligatory expenses item name for this month:';
-
-    let item;
-
-    do {
-        item = prompt(message);
-    } while ( !isCorrectString(item) );
-
-    return item;
-}
-
-function askExpenseAmount() {
-    const message = 'How much will it cost for this month?';
-    const defaultValue = 'Enter positive number value';
-
-    let expenseAmount;
-
-    do {
-        expenseAmount = prompt(message, defaultValue);
-    } while ( !isCorrectNumber(expenseAmount) );
-
-    return + parseFloat(expenseAmount).toFixed(2);
-}
-
-function hasExpenseItem(expenses, expenseName) {
-    return expenses[expenseName] !== undefined;
-}
-
-function askSaveAmount() {
-    let message = 'How much does it your accumulations?';
-    let defaultValue = 'Enter number value!';
-    let amount;
-
-    do {
-        amount = prompt(message, defaultValue);
-    } while ( !isCorrectNumber(amount) );
-
-    return + parseFloat(amount).toFixed(2);
-}
-
-function askSavePercent() {
-    let message = 'How much does it percent of your deposit?';
-    let defaultValue = 'Enter number value';
-    let percent;
-
-    do {
-        percent = prompt(message, defaultValue);
-    } while ( !isCorrectNumber(percent) );
-
-    return + parseFloat(percent).toFixed(2);
-}
-
-function askOptExpenseItem() {
-    const message = 'What is your optional expense item?';
-    let item;
-
-    do {
-        item = prompt(message);
-    } while ( !isCorrectString(item) );
-
-    return item;
-}
-
-// helper functions
-function isCorrectNumber(data) {
-    const minNumber = 0;
-    const maxNumber = 5000;
-
-    let isNumber = data !== null && data !== '' && !isNaN(data);
-    if ( ! isNumber ) {
-        alert('Enter number value!');
-        return false;
+    function askForCorrectDate(msg, def) {
+        return askFor(msg, def, hasCorrectTimeFormat);
     }
 
-    let isCorrectNumber = data > minNumber && data < maxNumber;
-    if ( ! isCorrectNumber ) {
-        alert('Enter number between ' + minNumber + ' and ' + maxNumber + '!');
-        return false;
+    function askForCorrectString(msg, def) {
+        return askFor(msg, def, isCorrectString);
     }
 
-    return true;
-}
+    function askFor(msg, def, check) {
+        let dt;
 
-function hasCorrectTimeFormat(time) {
-    const timeFormat = '^20[0-9]{2}-(0[0-9]{1}|1[0-2]{1})-([0-2]{1}[0-9]{1}|3[0-1]{1})$';
-    const regexpTime = RegExp(timeFormat);
+        do {
+            dt = prompt(msg, def);
+        } while ( !check(dt) );
 
-    return regexpTime.test(time);
-}
+        return dt;
 
-function isCorrectString(data) {
-    const allowedStringLength = 50;
+    }
+    
+}());
 
-    return (
-        typeof(data) === 'string' &&
-        data !== '' &&
-        data.length <= allowedStringLength
-    );
-}
 
