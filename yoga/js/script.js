e6fd3851f65825b22bbf4e8431ce74ffd367fed3
Vakhtang Tivadze <vaxotivadze@gmail.com>
2020-07-01 23:34:14 +0400

    Realize tabs for project yoaga

--- yoga/js/script.js
+++ yoga/js/script.js
@@ -3,23 +3,23 @@ window.addEventListener('DOMContentLoaded', function () {
     'use strict';
 
     let tabContainer = document.querySelector('.info-header');
-    let tab = document.querySelectAll('.info-header-tab');
-    let tabContent = document.querySelectorAll('info-tabcontent');
+    let tab = document.querySelectorAll('.info-header-tab');
+    let tabContent = document.querySelectorAll('.info-tabcontent');
 
     function hideAllTabContents() {
         for (let i = 0; i < tabContent.length; i++) {
-            console.log(i);
             tabContent[i].classList.remove('show');
             tabContent[i].classList.add('hide');
         }
     }
 
-    hideAllTabContents();
-
     function showTabContent(index) {
         tabContent[index].classList.remove('hide');
         tabContent[index].classList.add('show');
     }
+
+    hideAllTabContents();
+    showTabContent(0);
 
     tabContainer.addEventListener('click', function (event) {
         let target = event.target;
