7bc47c422a0212bbe8e16c6e3b22038f8d3301a0
Vakhtang Tivadze <vaxotivadze@gmail.com>
2020-07-03 03:39:48 +0400

    Realize timer

--- yoga/js/script.js
+++ yoga/js/script.js
@@ -6,10 +6,52 @@ window.addEventListener('DOMContentLoaded', function () {
         tabClass = '.info-header-tab',
         tabContentClass = '.info-tabcontent';
 
+    let timerContainerId = 'timer',
+        hoursClass = '.hours',
+        minutesClass = '.minutes',
+        secondsClass = '.seconds',
+        deadline = '2020-07-06';
+
     initiate();
 
     function initiate() {
         tabs(tabContainerClass, tabClass, tabContentClass);
+        timer(timerContainerId, hoursClass, minutesClass, secondsClass, deadline);
+    }
+
+    function timer(timerContainerId, hoursClass, minutesClass, secondsClass, deadline) {
+        let timerContainer = document.getElementById(timerContainerId);
+        let hoursElement = timerContainer.querySelector(hoursClass);
+        let minutesElement = timerContainer.querySelector(minutesClass);
+        let secondsElement = timerContainer.querySelector(secondsClass);
+
+        let timerIntervalId = setInterval(updateTimer, 1000, hoursElement, minutesElement, secondsElement);
+
+        function updateTimer(hoursElement, minutesElement, secondsElement) {
+            let timeData = getRemainingTimeData(deadline);
+
+            hoursElement.textContent = timeData.hours;
+            minutesElement.textContent = timeData.minutes;
+            secondsElement.textContent = timeData.seconds;
+
+            if (timeData.milliseconds <= 0) {
+                clearInterval(timerIntervalId);
+            }
+        }
+
+        function getRemainingTimeData(deadline) {
+            let milliseconds = Date.parse(deadline) - Date.parse(new Date());
+            let seconds = Math.floor(milliseconds / 1000) % 60;
+            let minutes = Math.floor(milliseconds / 1000 / 60) % 60;
+            let hours = Math.floor(milliseconds / 1000 / 60 / 60);
+
+            return {
+                milliseconds,
+                seconds,
+                minutes,
+                hours,
+            };
+        }
     }
 
     function tabs(tabContainerClass, tabClass, tabContentClass) {
