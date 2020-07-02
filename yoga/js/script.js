df1e8cc96bd3ba5c5aae7a8e0d048b9addf932b4
Vakhtang Tivadze <vaxotivadze@gmail.com>
2020-07-03 02:35:58 +0400

    Move tab functionality in separate function

--- yoga/js/script.js
+++ yoga/js/script.js
@@ -2,38 +2,51 @@ window.addEventListener('DOMContentLoaded', function () {
 
     'use strict';
 
-    let tabContainer = document.querySelector('.info-header');
-    let tab = document.querySelectorAll('.info-header-tab');
-    let tabContent = document.querySelectorAll('.info-tabcontent');
+    let tabContainerClass = '.info-header',
+        tabClass = '.info-header-tab',
+        tabContentClass = '.info-tabcontent';
 
-    function hideAllTabContents() {
-        for (let i = 0; i < tabContent.length; i++) {
-            tabContent[i].classList.remove('show');
-            tabContent[i].classList.add('hide');
-        }
+    initiate();
+
+    function initiate() {
+        tabs(tabContainerClass, tabClass, tabContentClass);
     }
 
-    function showTabContent(index) {
-        tabContent[index].classList.remove('hide');
-        tabContent[index].classList.add('show');
-    }
+    function tabs(tabContainerClass, tabClass, tabContentClass) {
+        let tabContainer = document.querySelector(tabContainerClass);
+        let tab = document.querySelectorAll(tabClass);
+        let tabContent = document.querySelectorAll(tabContentClass);
 
-    hideAllTabContents();
-    showTabContent(0);
-
-    tabContainer.addEventListener('click', function (event) {
-        let target = event.target;
-
-        if (target && target.classList.contains('info-header-tab')) {
-            hideAllTabContents();
-            
-            for (let i = 0; i < tab.length; i++) {
-                if (target == tab[i]) {
-                    showTabContent(i);
-                    return;
-                }
+        function hideAllTabContents() {
+            for (let i = 0; i < tabContent.length; i++) {
+                tabContent[i].classList.remove('show');
+                tabContent[i].classList.add('hide');
             }
         }
-    });
+
+        function showTabContent(index) {
+            tabContent[index].classList.remove('hide');
+            tabContent[index].classList.add('show');
+        }
+
+        hideAllTabContents();
+        showTabContent(0);
+
+        tabContainer.addEventListener('click', function (event) {
+            let target = event.target;
+
+            if (target && target.classList.contains(tabClass)) {
+                hideAllTabContents();
+                
+                for (let i = 0; i < tab.length; i++) {
+                    if (target == tab[i]) {
+                        showTabContent(i);
+                        return;
+                    }
+                }
+            }
+        });
+
+    }
 
 });
