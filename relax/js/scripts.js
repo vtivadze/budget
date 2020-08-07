260f6b9bc417467c2bd355b6f7cb964fbff3e5ca
Vakhtang Tivadze <vaxotivadze@gmail.com>
2020-08-08 01:02:19 +0400

    Realize modal window closing

--- relax/js/scripts.js
+++ relax/js/scripts.js
@@ -1,9 +1,19 @@
 $(document).ready(function() {
     $('[href="#tour"], .contact, .main_nav [href="#sheldure"]').on('click', function() {
-        $('.overlay').fadeTo(1000, 0.5);
+        // $('.overlay').fadeTo(1000, 0.5);
+        $('.overlay').fadeToggle(1000);
         $('.modal').css({'display': 'block', 'top': '-500px'}).animate(
             {
                 'top': '0',
+            }, 1500
+        );
+    });
+
+    $('.modal .close').on('click', function() {
+        $('.overlay').fadeToggle(1000);
+        $('.modal').animate(
+            {
+                'top': '-500px',
             }, 1500
         );
     });
