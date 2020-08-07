5239077804424d66578e32ab678c2639a54cc645
Vakhtang Tivadze <vaxotivadze@gmail.com>
2020-08-08 00:54:02 +0400

    Realize modal window appearing

--- relax/js/scripts.js
+++ relax/js/scripts.js
@@ -1,3 +1,10 @@
 $(document).ready(function() {
-    console.log('document is loaded');
+    $('[href="#tour"], .contact, .main_nav [href="#sheldure"]').on('click', function() {
+        $('.overlay').fadeTo(1000, 0.5);
+        $('.modal').css({'display': 'block', 'top': '-500px'}).animate(
+            {
+                'top': '0',
+            }, 1500
+        );
+    });
 });
